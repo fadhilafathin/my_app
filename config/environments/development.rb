b2a2990ec2114627fee4cc2dc013a288bf6200d8
fadhilafathin <95992836+fadhilafathin@users.noreply.github.com>
2024-06-12 08:08:23 +0000

    melakukan generate kolom user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "musical-umbrella-q764w7rj529j7r-3000.app.github.dev"
+  config.hosts << "studious-space-dollop-jp5wv7w5qjphp765-3000.app.github.dev"
 end
